beacd7f8744c3626957e1922323e066000bd4769
mustafaabdullabhaik <mustafaabdullabhaik@sahaj.ai>
2024-02-15 10:29:14 +0530

    top row completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,31 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val 
+    val ticket = ArrayList<ArrayList<String>>()
+    var numbersAnnounced = ArrayList<String>()
+    var numbersGained = 0
+    repeat(3){
+        ticket.add(readln().split(',') as ArrayList<String>)
+    }
+    numbersAnnounced = readln().split(", ") as ArrayList<String>
+    val claim = readln()
+    var flag=false
+
+    if (claim=="Top Row"){
+        var numberCount = 0
+        while (numberCount<numbersAnnounced.size){
+            if (numbersAnnounced[numberCount] in ticket[0]){
+                numbersGained+=1
+            }
+            if (numberCount==numbersAnnounced.size-1 && numbersGained==5){
+                flag=true
+                break
+            }
+            if (numbersGained==5){
+                break
+            }
+            numberCount+=1
+
+        }
+    }
 }
