1cd1b98c69180fc56b912a5258d337f2e15b34bd
Soham Parate <sohamparate1905@gmail.com>
2024-02-15 10:21:04 +0530

    Initial commit with half of the test case written

---
+++ src/test/kotlin/TambolaTest.kt
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test
+
+class TambolaTest {
+    @Test
+    fun `the number announced should get marked as true in the ticket`() {
+        val ticket = Ticket((mapOf(
+            1 to listOf(Pair(4, false), Pair(16, false), Pair(48, false), Pair(63, false), Pair(76, false)),
+            2 to listOf(Pair(7, false), Pair(23, false), Pair(38, false), Pair(52, false), Pair(80, false)),
+            3 to listOf(Pair(9, false), Pair(25, false), Pair(56, false), Pair(64, false), Pair(83, false)),
+        )))
+        val announcedNumber = 4
+        for (row in ticket) {
+
+        }
+    }
+}
