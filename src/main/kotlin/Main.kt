5ca89c415e558fbc71ab08524b396011eb2f16a6
mustafaabdullabhaik <mustafaabdullabhaik@sahaj.ai>
2024-02-15 10:30:24 +0530

    tambola claim validator completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -30,4 +30,45 @@ fun main() {
 
         }
     }
+    else if (claim=="Bottom Row"){
+        var numberCount = 0
+        while (numberCount<numbersAnnounced.size){
+            if (numbersAnnounced[numberCount] in ticket[2]){
+                numbersGained+=1
+            }
+            if (numberCount==numbersAnnounced.size-1 && numbersGained==5){
+                flag=true
+                break
+            }
+            if (numbersGained==5){
+                break
+            }
+            numberCount+=1
+
+        }
+    }
+    else if (claim=="Early Five"){
+        var numberCount = 0
+        while (numberCount<numbersAnnounced.size){
+            if (numbersAnnounced[numberCount] in ticket[0] || numbersAnnounced[numberCount] in ticket[1]
+                || numbersAnnounced[numberCount] in ticket[2]){
+                numbersGained+=1
+            }
+            if (numberCount==numbersAnnounced.size-1 && numbersGained==5){
+                flag=true
+                break
+            }
+            if (numbersGained==5){
+                break
+            }
+            numberCount+=1
+
+        }
+    }
+    if (flag){
+        println("Accepted")
+    }
+    else{
+        println("Rejected")
+    }
 }
