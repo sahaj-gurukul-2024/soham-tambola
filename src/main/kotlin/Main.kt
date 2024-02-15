4f427304cb7a53ccd9a0465753f4c4c853e56aa7
mustafaabdullabhaik <mustafaabdullabhaik@sahaj.ai>
2024-02-15 13:00:26 +0530

    Added Middle Row and Full House functionality

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,82 +4,64 @@ package org.example
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 class Tambola(){
-    private var flag=false
     private var numbersGained = 0
 
-    fun validate(ticket:ArrayList<ArrayList<String>>, numbersAnnounced:ArrayList<String>,
-                 claim:String): Boolean {
-        if (claim=="Top Row"){
-            var numberCount = 0
-            while (numberCount<numbersAnnounced.size){
-                if (numbersAnnounced[numberCount] in ticket[0]){
-                    numbersGained+=1
-                }
-                if (numberCount==numbersAnnounced.size-1 && numbersGained==5){
-                    flag=true
-                    break
-                }
-                if (numbersGained==5){
-                    break
-                }
-                numberCount+=1
+    fun rowValidation(ticket:ArrayList<ArrayList<String>>, numbersAnnounced:ArrayList<String>,
+                 row_number:Int): Boolean {
 
+        var numberCount = 0
+        while (numberCount<numbersAnnounced.size){
+            if (numbersAnnounced[numberCount] in ticket[row_number]){
+                numbersGained+=1
             }
-        }
-        else if (claim=="Bottom Row"){
-            var numberCount = 0
-            while (numberCount<numbersAnnounced.size){
-                if (numbersAnnounced[numberCount] in ticket[2]){
-                    numbersGained+=1
-                }
-                if (numberCount==numbersAnnounced.size-1 && numbersGained==5){
-                    flag=true
-                    break
-                }
-                if (numbersGained==5){
-                    break
-                }
-                numberCount+=1
+            if (numberCount==numbersAnnounced.size-1 && numbersGained==5){
+                return true
+            }
+            if (numbersGained==5){
+                return false
+            }
+            numberCount+=1
 
-            }
         }
-        else if (claim=="Early Five"){
-            var numberCount = 0
-            while (numberCount<numbersAnnounced.size){
-                if (numbersAnnounced[numberCount] in ticket[0] || numbersAnnounced[numberCount] in ticket[1]
-                    || numbersAnnounced[numberCount] in ticket[2]){
-                    numbersGained+=1
-                }
-                if (numberCount==numbersAnnounced.size-1 && numbersGained==5){
-                    flag=true
-                    break
-                }
-                if (numbersGained==5){
-                    break
-                }
-                numberCount+=1
+        return false
+    }
 
+    fun earlyNumberValidation(ticket:ArrayList<ArrayList<String>>, numbersAnnounced:ArrayList<String>,
+                              number:Int):Boolean{
+        var numberCount = 0
+        while (numberCount<numbersAnnounced.size){
+            if (numbersAnnounced[numberCount] in ticket[0] || numbersAnnounced[numberCount] in ticket[1] ||
+                numbersAnnounced[numberCount] in ticket[2]){
+                numbersGained+=1
             }
+            if (numberCount==numbersAnnounced.size-1 && numbersGained==number){
+                return true
+            }
+            if (numbersGained==number){
+                return false
+            }
+            numberCount+=1
+
         }
-        return flag
+        return false
     }
 
 
 }
 fun main() {
-    val ticket = ArrayList<ArrayList<String>>()
-    var numbersAnnounced = ArrayList<String>()
-
-    repeat(3){
-        ticket.add(readln().split(',') as ArrayList<String>)
-    }
-    numbersAnnounced = readln().split(", ") as ArrayList<String>
-    val claim = readln()
-    val tambola = Tambola()
-    if (tambola.validate(ticket, numbersAnnounced, claim)){
-        println("Accepted")
-    }
-    else{
-        println("Rejected")
-    }
+//    val ticket = ArrayList<ArrayList<String>>()
+//    var numbersAnnounced = ArrayList<String>()
+//
+//    repeat(3){
+//        ticket.add(readln().split(',') as ArrayList<String>)
+//    }
+//    numbersAnnounced = readln().split(", ") as ArrayList<String>
+//    val claim = readln()
+//    val tambola = Tambola()
+//    if (tambola.validate(ticket, numbersAnnounced, claim)){
+//        println("Accepted")
+//    }
+//    else{
+//        println("Rejected")
+//    }
 }
