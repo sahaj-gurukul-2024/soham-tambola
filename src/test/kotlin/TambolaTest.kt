0974a2a5605d8d6c9387354c977d467cef77b1e9
mustafaabdullabhaik <mustafaabdullabhaik@sahaj.ai>
2024-02-15 11:18:04 +0530

    added tests

---
+++ src/test/kotlin/TambolaTest.kt
@@ -0,0 +1,97 @@
+import org.example.Tambola
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class TambolaTest {
+    @Test
+    fun `Top Row validation for acceptance claim`(){
+        val tambola = Tambola()
+
+        val ticket = ArrayList<ArrayList<String>>()
+        ticket.add("4,16,_,_,48,_,63,76,_".split(',') as ArrayList<String>)
+        ticket.add("7,_,23,38,_,52,_,_,80".split(',') as ArrayList<String>)
+        ticket.add("9,_,25,_,_,56,64,_,83".split(',') as ArrayList<String>)
+
+        val numberAnnounced = ArrayList<String>("4, 16, 17, 63, 48, 76".split(", "))
+
+        val claim = "Top Row"
+        assertTrue(tambola.validate(ticket, numberAnnounced, claim))
+    }
+
+    @Test
+    fun `Top Row validation for rejection claim`(){
+        val tambola = Tambola()
+
+        val ticket = ArrayList<ArrayList<String>>()
+        ticket.add("4,16,_,_,48,_,63,76,_".split(',') as ArrayList<String>)
+        ticket.add("7,_,23,38,_,52,_,_,80".split(',') as ArrayList<String>)
+        ticket.add("9,_,25,_,_,56,64,_,83".split(',') as ArrayList<String>)
+
+        val numberAnnounced = ArrayList<String>("4, 16, 17, 63, 48, 76, 12".split(", "))
+
+        val claim = "Top Row"
+        assertFalse(tambola.validate(ticket, numberAnnounced, claim))
+    }
+
+
+    @Test
+    fun `Bottom Row validation for acceptance claim`(){
+        val tambola = Tambola()
+
+        val ticket = ArrayList<ArrayList<String>>()
+        ticket.add("4,16,_,_,48,_,63,76,_".split(',') as ArrayList<String>)
+        ticket.add("7,_,23,38,_,52,_,_,80".split(',') as ArrayList<String>)
+        ticket.add("9,_,25,_,_,56,64,_,83".split(',') as ArrayList<String>)
+
+        val numberAnnounced = ArrayList<String>("9, 16, 17, 56, 48, 83, 25, 64".split(", "))
+
+        val claim = "Bottom Row"
+        assertTrue(tambola.validate(ticket, numberAnnounced, claim))
+    }
+
+    @Test
+    fun `Bottom Row validation for rejection claim`(){
+        val tambola = Tambola()
+
+        val ticket = ArrayList<ArrayList<String>>()
+        ticket.add("4,16,_,_,48,_,63,76,_".split(',') as ArrayList<String>)
+        ticket.add("7,_,23,38,_,52,_,_,80".split(',') as ArrayList<String>)
+        ticket.add("9,_,25,_,_,56,64,_,83".split(',') as ArrayList<String>)
+
+        val numberAnnounced = ArrayList<String>("9, 16, 17, 56, 48, 83, 25, 56, 43".split(", "))
+
+        val claim = "Bottom Row"
+        assertFalse(tambola.validate(ticket, numberAnnounced, claim))
+    }
+
+    @Test
+    fun `Early Five validation for acceptance claim`(){
+        val tambola = Tambola()
+
+        val ticket = ArrayList<ArrayList<String>>()
+        ticket.add("4,16,_,_,48,_,63,76,_".split(',') as ArrayList<String>)
+        ticket.add("7,_,23,38,_,52,_,_,80".split(',') as ArrayList<String>)
+        ticket.add("9,_,25,_,_,56,64,_,83".split(',') as ArrayList<String>)
+
+        val numberAnnounced = ArrayList<String>("48 ,17 ,12, 52, 25, 62, 64, 56".split(", "))
+
+        val claim = "Early Five"
+        assertTrue(tambola.validate(ticket, numberAnnounced, claim))
+    }
+
+    @Test
+    fun `Early Five validation for rejection claim`(){
+        val tambola = Tambola()
+
+        val ticket = ArrayList<ArrayList<String>>()
+        ticket.add("4,16,_,_,48,_,63,76,_".split(',') as ArrayList<String>)
+        ticket.add("7,_,23,38,_,52,_,_,80".split(',') as ArrayList<String>)
+        ticket.add("9,_,25,_,_,56,64,_,83".split(',') as ArrayList<String>)
+
+        val numberAnnounced = ArrayList<String>("48 ,17 ,12, 52, 25, 62, 64, 56".split(", "))
+
+        val claim = "Early Five"
+        assertFalse(tambola.validate(ticket, numberAnnounced, claim))
+    }
+}
