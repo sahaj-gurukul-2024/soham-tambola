51366012671a1ba215cadb99f25ffc3bb1250dec
Soham Parate <sohamparate1905@gmail.com>
2024-02-15 17:37:54 +0530

    Added Tests for early five validation

--- src/test/kotlin/TambolaTest.kt
+++ src/test/kotlin/TambolaTest.kt
@@ -1,5 +1,6 @@
 import org.example.Tambola
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class TambolaTest {
@@ -21,7 +22,7 @@ class TambolaTest {
     }
 
     @Test
-    fun `check if top row claim is valid`() {
+    fun `should accept if top row claim is valid`() {
         val tambola = Tambola(
             (mutableMapOf(
                 1 to mutableMapOf(4 to false, 16 to false, 48 to false, 63 to false, 76 to false),
@@ -43,10 +44,11 @@ class TambolaTest {
 
         val claim = tambola.validClaim("Top Row", ticket, lastAnnouncedNumber)
 
-        assertTrue(claim == "Accepted")
+        assertTrue("Accepted" == claim)
     }
 
-    fun `check if bottom row claim is valid`() {
+    @Test
+    fun `should reject if top row claim is invalid`() {
         val tambola = Tambola(
             (mutableMapOf(
                 1 to mutableMapOf(4 to false, 16 to false, 48 to false, 63 to false, 76 to false),
@@ -63,12 +65,113 @@ class TambolaTest {
         tambola.markNumber(ticket, 16)
         tambola.markNumber(ticket, 76)
         tambola.markNumber(ticket, 48)
+        tambola.markNumber(ticket, 59)
 
-        val lastAnnouncedNumber = 48
+        val lastAnnouncedNumber = 59
 
         val claim = tambola.validClaim("Top Row", ticket, lastAnnouncedNumber)
 
-        assertTrue(claim == "Accepted")
+        assertTrue("Rejected" == claim)
     }
 
+    @Test
+    fun `should accept if the bottom row claim is valid`() {
+        val tambola = Tambola(
+            (mutableMapOf(
+                1 to mutableMapOf(4 to false, 16 to false, 48 to false, 63 to false, 76 to false),
+                2 to mutableMapOf(7 to false, 23 to false, 38 to false, 52 to false, 80 to false),
+                3 to mutableMapOf(9 to false, 25 to false, 56 to false, 64 to false, 83 to false),
+            ))
+        )
+        val ticket = tambola.getTicket()
+        tambola.markNumber(ticket, 9)
+        tambola.markNumber(ticket, 25)
+        tambola.markNumber(ticket, 26)
+        tambola.markNumber(ticket, 27)
+        tambola.markNumber(ticket, 56)
+        tambola.markNumber(ticket, 64)
+        tambola.markNumber(ticket, 83)
+
+        val lastAnnouncedNumber = 83
+
+        val claim = tambola.validClaim("Bottom Row", ticket, lastAnnouncedNumber)
+
+        assertTrue("Accepted" == claim)
+    }
+
+    @Test
+    fun `should reject if the bottom row claim is invalid`() {
+        val tambola = Tambola(
+            (mutableMapOf(
+                1 to mutableMapOf(4 to false, 16 to false, 48 to false, 63 to false, 76 to false),
+                2 to mutableMapOf(7 to false, 23 to false, 38 to false, 52 to false, 80 to false),
+                3 to mutableMapOf(9 to false, 25 to false, 56 to false, 64 to false, 83 to false),
+            ))
+        )
+        val ticket = tambola.getTicket()
+        tambola.markNumber(ticket, 9)
+        tambola.markNumber(ticket, 25)
+        tambola.markNumber(ticket, 26)
+        tambola.markNumber(ticket, 27)
+        tambola.markNumber(ticket, 56)
+        tambola.markNumber(ticket, 64)
+        tambola.markNumber(ticket, 83)
+        tambola.markNumber(ticket, 84)
+
+        val lastAnnouncedNumber = 84
+
+        val claim = tambola.validClaim("Bottom Row", ticket, lastAnnouncedNumber)
+
+        assertTrue("Rejected" == claim)
+    }
+
+    @Test
+    fun `should accept if the early five claim is valid`() {
+        val tambola = Tambola(
+            (mutableMapOf(
+                1 to mutableMapOf(4 to false, 16 to false, 48 to false, 63 to false, 76 to false),
+                2 to mutableMapOf(7 to false, 23 to false, 38 to false, 52 to false, 80 to false),
+                3 to mutableMapOf(9 to false, 25 to false, 56 to false, 64 to false, 83 to false),
+            ))
+        )
+        val ticket = tambola.getTicket()
+        tambola.markNumber(ticket, 4)
+        tambola.markNumber(ticket, 16)
+        tambola.markNumber(ticket, 48)
+        tambola.markNumber(ticket, 52)
+        tambola.markNumber(ticket, 80)
+
+        val lastAnnouncedNumber = 80
+
+        val claim = tambola.validClaim("Early Five", ticket, lastAnnouncedNumber)
+
+        assertTrue("Accepted" == claim)
+    }
+
+    @Test
+    fun `should reject if the early five claim is invalid`() {
+        val tambola = Tambola(
+            (mutableMapOf(
+                1 to mutableMapOf(4 to false, 16 to false, 48 to false, 63 to false, 76 to false),
+                2 to mutableMapOf(7 to false, 23 to false, 38 to false, 52 to false, 80 to false),
+                3 to mutableMapOf(9 to false, 25 to false, 56 to false, 64 to false, 83 to false),
+            ))
+        )
+        val ticket = tambola.getTicket()
+        tambola.markNumber(ticket, 4)
+        tambola.markNumber(ticket, 16)
+        tambola.markNumber(ticket, 48)
+        tambola.markNumber(ticket, 52)
+        tambola.markNumber(ticket, 80)
+        tambola.markNumber(ticket, 81)
+
+
+        val lastAnnouncedNumber = 81
+
+        val claim = tambola.validClaim("Early Five", ticket, lastAnnouncedNumber)
+
+        assertTrue("Rejected" == claim)
+    }
+
+    
 }
