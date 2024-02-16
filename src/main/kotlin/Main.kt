596417971d1186f69057b6ad4fda9aedfec72df8
Soham Parate <sohamparate1905@gmail.com>
2024-02-16 13:47:34 +0530

    refactor: Extract Tambola out of main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,66 +1,18 @@
 package org.example
 
-class Tambola(){
-    private var crossedNumberCount = 0
-
-    fun rowValidation(
-        ticket: ArrayList<ArrayList<String>>,
-        numbersAnnounced: ArrayList<String>,
-        rowNumber: Int
-    ): Boolean {
-        var currentNumberCount = 0
-
-        while (currentNumberCount < numbersAnnounced.size) {
-            if (numbersAnnounced[currentNumberCount] in ticket[rowNumber]) {
-                crossedNumberCount += 1
-            }
-            if (currentNumberCount == numbersAnnounced.size - 1 && crossedNumberCount == 5) {
-                return true
-            }
-            if (crossedNumberCount == 5) {
-                return false
-            }
-            currentNumberCount += 1
-        }
-        return false
-    }
-
-    fun earlyFiveOrFullHouseValidation(
-        ticket: ArrayList<ArrayList<String>>, numbersAnnounced: ArrayList<String>,
-        number: Int
-    ): Boolean {
-        var numberCount = 0
-        while (numberCount < numbersAnnounced.size) {
-            if (numbersAnnounced[numberCount] in ticket[0] || numbersAnnounced[numberCount] in ticket[1] ||
-                numbersAnnounced[numberCount] in ticket[2]
-            ) {
-                crossedNumberCount += 1
-            }
-            if (numberCount == numbersAnnounced.size - 1 && crossedNumberCount == number) {
-                return true
-            }
-            if (crossedNumberCount == number) {
-                return false
-            }
-            numberCount += 1
-        }
-        return false
-    }
-}
 fun main() {
-//    val ticket = ArrayList<ArrayList<String>>()
-//    var numbersAnnounced = ArrayList<String>()
-//
-//    repeat(3){
-//        ticket.add(readln().split(',') as ArrayList<String>)
-//    }
-//    numbersAnnounced = readln().split(", ") as ArrayList<String>
-//    val claim = readln()
-//    val tambola = Tambola()
-//    if (tambola.validate(ticket, numbersAnnounced, claim)){
-//        println("Accepted")
-//    }
-//    else{
-//        println("Rejected")
-//    }
+    val ticket = ArrayList<ArrayList<String>>()
+
+    repeat(3){
+        ticket.add(readln().split(',') as ArrayList<String>)
+    }
+    val numbersAnnounced: ArrayList<String> = readln().split(", ") as ArrayList<String>
+    val claim = readln()
+    val tambola = Tambola()
+    if (tambola.valid(ticket, numbersAnnounced, claim)){
+        println("Accepted")
+    }
+    else{
+        println("Rejected")
+    }
 }
