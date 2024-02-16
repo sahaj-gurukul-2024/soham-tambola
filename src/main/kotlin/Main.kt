dc4264d5082e84adc153bc3435956230d1797950
Soham Parate <sohamparate1905@gmail.com>
2024-02-16 16:46:32 +0530

    fix: change main flow to call validateClaim

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,22 @@
 package org.example
 
 fun main() {
-//    val ticket = ArrayList<ArrayList<String>>()
-//
-//    repeat(3){
-//        ticket.add(readln().split(',') as ArrayList<String>)
-//    }
-//    val numbersAnnounced: ArrayList<String> = readln().split(", ") as ArrayList<String>
-//    val claim = readln()
-//    val tambola = Tambola()
-//    if (tambola.valid(ticket, numbersAnnounced, claim)){
-//        println("Accepted")
-//    }
-//    else{
-//        println("Rejected")
-//    }
+    val tambola = Tambola()
+
+    val ticket = TambolaTicket(
+        arrayOf(4, 16, 48, 63, 76),
+        arrayOf(7, 23, 38, 52, 80),
+        arrayOf(9, 25, 56, 64, 83),
+    )
+
+    val numbersAnnounced = arrayListOf(4, 16, 48, 63, 76, 7, 23, 38, 52, 80, 9, 25, 56, 64, 83)
+
+    val claim = "FullHouse"
+
+    if (tambola.validateClaim(ticket, numbersAnnounced, claim)){
+        println("Accepted")
+    }
+    else{
+        println("Rejected")
+    }
 }
